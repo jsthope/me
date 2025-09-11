c632b3d59d8761f2937eda384f3d680cc56394cc
JstHope <68657498+JstHope@users.noreply.github.com>
2025-09-11 21:14:16 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jsthope.github.io/",
-	base: "/me",
+	site: "https://jsthope.xyz",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
